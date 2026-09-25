83afed9495f3f4111844fc59d19e08ff0fb242d8
Ashok <ashok.koduru888@gmail.com>
2026-09-25 16:14:41 +0530

    modified

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -29,7 +29,7 @@ const HeroContent = () => {
                                         className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
                                     >
                                         <BsStars className="text-[#b49bff] mr-[10px] h-5 w-5" />
-                                        <h1 className="Welcome-text text-[13px]">Java Developer</h1>
+                                        <h1 className="Welcome-text text-[13px]">Data Analyst</h1>
                                     </motion.div>
                                 )}
                             </InView>
